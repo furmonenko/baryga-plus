5a35da2ea93075d36a933840e5a5379e9dc2076f
Zakharii Furmanets <zfurmanec@gmail.com>
2024-07-28 01:46:35 +0200

    change ngrok

--- resetWebhook.js
+++ resetWebhook.js
@@ -3,7 +3,7 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 require('dotenv').config(); // Завантаження змінних середовища з файлу .env
 
 const TELEGRAM_API_URL = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`;
-const NGROK_URL = process.env.NGROK_URL; // Додайте ваш NGROK URL до .env файлу
+const NGROK_URL = process.env.SERVER_IP; // Додайте ваш NGROK URL до .env файлу
 
 // Функція для видалення вебхука
 async function deleteWebhook() {
